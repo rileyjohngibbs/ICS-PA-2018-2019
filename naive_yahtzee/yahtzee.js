331860f69f6b3289815f526fda9179b1f3c23f68
Riley Gibbs <rileyjohngibbs@gmail.com>
2019-02-22 01:37:09 -0800

    Implemented some scoring and turn reset

--- naive_yahtzee/yahtzee.js
+++ naive_yahtzee/yahtzee.js
@@ -30,6 +30,10 @@ const modelDice = [new Die(), new Die(), new Die(), new Die(), new Die()];
 // Get view elements
 const domDice = document.querySelectorAll('.die');
 
+let rollCount = 0;
+const counter = document.getElementById('counter');
+let score = 0;
+
 // Connect models to view elements
 for (let i = 0; i < modelDice.length; i++) {
   domDice[i].modelDie = modelDice[i];
@@ -44,6 +48,59 @@ function rollDice() {
   modelDice.forEach((die) => {
     !die.locked ? die.roll().updateDom() : '';
   });
+  rollCount++;
+  counter.innerHTML = rollCount;
+  if (rollCount >= 3) {
+    document.getElementById('roll-btn').disabled = true;
+  }
+  const faces = modelDice.map((die) => die.face);
+  checkScoring(faces);
+}
+
+function reset() {
+  rollCount = 0;
+  modelDice.forEach((die) => {die.locked = false});
+  rollDice();
+  document.getElementById('roll-btn').disabled = false;
+}
+
+function checkScoring(arr) {
+  document.getElementById('three-kind-btn').disabled = !hasSet(arr, 3);
+  document.getElementById('four-kind-btn').disabled = !hasSet(arr, 4);
+  document.getElementById('yahtzee-btn').disabled = !hasSet(arr, 5);
+}
+
+function hasSet(arr, num) {
+  const counts = new Array(6).fill(0);
+  for (let a of arr) {
+    counts[a]++;
+  }
+  for (let c of counts) {
+    if (c >= num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function scoreThreeKind() {
+  const sum = modelDice.reduce((a, b) => a + b.face, 0);
+  score += sum;
+  document.getElementById('score').innerHTML = score;
+  reset();
+}
+
+function scoreFourKind() {
+  const sum = modelDice.reduce((a, b) => a + b.face, 0);
+  score += sum + 10;
+  document.getElementById('score').innerHTML = score;
+  reset();
+}
+
+function scoreYahtzee() {
+  score += 50;
+  document.getElementById('score').innerHTML = score;
+  reset();
 }
 
 // Initial roll
