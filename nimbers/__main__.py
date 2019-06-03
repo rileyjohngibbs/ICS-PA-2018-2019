b04c373652f466f7371d2f3c4e6925834c50035e
Riley John Gibbs <rileyjohngibbs@gmail.com>
2019-06-03 10:11:40 -0700

    Add missing import; ask for starting nimbers

--- nimbers/__main__.py
+++ nimbers/__main__.py
@@ -1,6 +1,7 @@
 from nimbers.nimbers import Nimbers
 from nimbers.human import Human
 from nimbers.randomplayer import RandomPlayer
+from nimbers.aiplayer import ArtificialIntelligence
 
 
 if __name__ == "__main__":
@@ -8,10 +9,18 @@ if __name__ == "__main__":
     while not player_count.isdigit() or not int(player_count) in [1, 2]:
         player_count = input("How many human players? [1, 2] ")
     if player_count == "1":
-        game = Nimbers(Human(input("Name? ")), RandomPlayer())
+        try:
+            ai = ArtificialIntelligence()
+            ai.make_move(5, 3)
+            game = Nimbers(Human(input("Name? ")), ai)
+        except NotImplementedError:
+            game = Nimbers(Human(input("Name? ")), RandomPlayer())
     else:
         game = Nimbers(
             Human(input("Name of first player? ")),
             Human(input("Name of second player? "))
         )
-    game.play()
+    nimber_count = "A"
+    while not nimber_count.isdigit() or nimber_count == "0":
+        nimber_count = input("How many starting nimbers? ")
+    game.play(int(nimber_count))
