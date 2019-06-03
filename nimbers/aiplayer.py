a2521b65672235702e5fa84b16e2280bbd22dabb
Riley John Gibbs <rileyjohngibbs@gmail.com>
2019-06-03 09:56:44 -0700

    Add AI player class

---
+++ nimbers/aiplayer.py
@@ -0,0 +1,11 @@
+class ArtificialIntelligence(object):
+
+    def __init__(self, name="Andy the Android"):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+    def make_move(self, nimbers, max_take):
+        # TODO: Implement this function and return an integer
+        raise NotImplementedError()
