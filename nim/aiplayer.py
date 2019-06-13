c0842a623c7a23f943e70b3c8a68684302a7f2db
Riley Gibbs <rileyjohngibbs@gmail.com>
2019-06-13 09:43:02 -0700

    Create perfect doubling nim AI

--- nim/aiplayer.py
+++ nim/aiplayer.py
@@ -1,3 +1,6 @@
+import math
+
+
 class ArtificialIntelligence(object):
 
     def __init__(self, name="Andy the Android"):
@@ -7,5 +10,26 @@ class ArtificialIntelligence(object):
         return self.name
 
     def make_move(self, nimbers, max_take):
-        # TODO: Implement this function and return an integer
-        raise NotImplementedError()
+        if nimbers <= max_take:
+            return nimbers
+        target = self.find_highest_fib(nimbers)
+        needed = nimbers - target
+        if needed <= max_take:
+            return max(1, min(needed, math.ceil(nimbers/3) - 1))
+        else:
+            return self.make_move(needed, max_take)
+
+    def find_highest_fib(self, top):
+        gen = self.fib_gen()
+        fib = 0
+        candidate = next(gen)
+        while candidate < top:
+            fib = candidate
+            candidate = next(gen)
+        return fib
+
+    def fib_gen(self):
+        a, b = 0, 1
+        while True:
+            yield b
+            a, b = b, a + b
